3ab0685b9979b3503e5345746acd2d92215150c4
Eduardo Ucan <delacruzmari1411@gmail.com>
2022-06-30 14:08:32 -0500

    inicio

---
+++ resources/views/post.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">  
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post->title}}</h5>
+                    <p class="card-text">
+                        {{ $post->body}}
+                    </p>
+                    <p>
+                        <em>
+                            $ndash; {{ $post->user->email }}
+                        </em>
+                        {{ $post->updated_at->format('d M Y') }}
+                    </p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+@endsection
